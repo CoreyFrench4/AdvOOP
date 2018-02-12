8b33ca91587e1fe6f14a12ac306fb5f9abd5d341
coreyfrench4 <corey.french@hotmail.com>
2018-02-12 16:03:37 +1100

    IceT Generics

---
+++ Assets/1-Generics/Scripts/CustomList.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Generics
+{
+    public class CustomList<IceT> 
+    {
+        private IceT[] data;
+        public int amount;
+
+        public CustomList() { amount = 0; }
+
+        public void Add(IceT item)
+        {
+            //Create new array of amount + 1
+            IceT[] cache = new IceT[amount + 1];
+            //check all existing items to new array
+            for(int i = 0; i < data.Length; i++)
+            {
+                //copy each element to cache
+                cache[i] = data[i];
+            }
+        }
+       
+    }
+}
